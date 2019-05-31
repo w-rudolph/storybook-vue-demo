15bce9dccc44a357bf0af63807b56b09399e72ea
wangrujiang <wangrujiang@100.me>
2019-05-31 17:16:39 +0800

    update

--- stories/test.vue
+++ stories/test.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>{{message}}</div>
+    <div>
+        <p>{{message}}</p>
+        <el-button @click="handleClick('primary')">Go to `primary` case</el-button>
+        <el-button @click="handleClick('success')">Go to `success` case</el-button>
+    </div>
 </template>
 
 <script>
 import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 export default {
     data() {
         return {
@@ -17,10 +22,13 @@ export default {
                 this.message = Math.random().toString(16).slice(2);
                 action('Tick')(this.message);
             }, 1000);
+        },
+        handleClick(name) {
+            linkTo('button', name)();
         }
     },
     created() {
-       this.tick();
+        // this.tick();
     },
     beforeDestroy() {
         clearInterval(this.timer);
